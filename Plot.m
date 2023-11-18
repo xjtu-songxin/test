3ea17811102a97599a6f370feadd7ad96a7e9ba4
xjtu-songxin <simon.sxin@gmail.com>
2023-11-18 16:50:40 +0800

    plot function

---
+++ Plot.m
@@ -0,0 +1,28 @@
+%%% -------------------------------------------------- %%%
+%%% Author: Denys Dutykh, CNRS -- LAMA, Univ of Savoie %%%
+%%% E-mail: Denys.Dutykh@univ-savoie.fr                %%%
+%%% Web:    http://www.denys-dutykh.com/               %%%
+%%% Blog:   http://dutykh.github.io/                   %%%
+%%% GitHub: https://github.com/dutykh/                 %%%
+%%% -------------------------------------------------- %%%
+
+function Plot (Om, t, frame)
+
+	global dy Lx Ly X Y filename
+	
+	surf(X, Y, Om), grid off
+	shading interp;
+	colormap(jet); cc = colorbar;
+    xlim([-Lx Lx]); ylim([-Ly+dy Ly]); caxis([-1 1]);
+    xlabel('$x$', 'interpreter', 'latex', 'fontsize', 12);
+    ylabel('$y$', 'interpreter', 'latex', 'fontsize', 12, 'Rotation', 1);
+    xlabel(cc, '$\omega(x,y,t)$', 'interpreter', 'latex', 'fontsize', 12, 'Rotation', 90);
+    view([0 90]);
+
+    title (['Vorticity distribution at t = ',num2str(t,'%4.2f')], 'interpreter', 'latex', 'fontsize', 12);
+
+    set(gcf, 'Color', 'w');
+    drawnow
+
+    save(filename + "/" + string(t) + ".mat", 'Om')
+end % Plot ()
